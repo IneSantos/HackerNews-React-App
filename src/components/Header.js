5dc2c01a276ec476961501c5032f23531e4b4fae
Inês Carneiro <inescarneiro@Iness-MBP.home>
2020-07-08 16:40:20 +0100

    Search func added

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,10 +10,14 @@ class Header extends Component {
         return (
           <div className="flex pa1 justify-between nowrap orange">
             <div className="flex flex-fixed black">
-              <div className="fw7 mr1">Hacker News</div>
-              <Link to="/" className="ml1 no-underline black">
+                <div className="fw7 mr1">Hacker News</div>
+                <Link to="/" className="ml1 no-underline black">
                 new
-              </Link>
+                </Link>
+                <div className="ml1">|</div>
+                <Link to="/search" className="ml1 no-underline black">
+                search
+                </Link>
               {authToken && (
                 <div className="flex">
                   <div className="ml1">|</div>
